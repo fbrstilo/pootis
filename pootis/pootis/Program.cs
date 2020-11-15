7638267e07083bcca1e3e11b26c2e361589ba2c9
fbrstilo <fbrstilo@gmail.com>
2020-11-15 19:00:23 +0100

    Added initial project architecture

---
+++ pootis/pootis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pootis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
